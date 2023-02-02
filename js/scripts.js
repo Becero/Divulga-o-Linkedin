ff75d22c420b4ca12e9ea5d0e2e213660568b027
Luis Guilherme <51214414+Becero@users.noreply.github.com>
2023-02-02 10:38:58 -0300

    apenas atualizando

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    
+    //Scroll para seçoes
+
+    let navBtn=$('.nav-item');
+
+    let bannerSection = $('#mainSlider');
+    let aboutSection = $('#about-area');
+    let serviceSection = $('#services-area');
+    let contactSection = $('#contact-area');    
+
+    let scrollTo='';
+
+    $(navBtn).click(function(){
+        let btnId = $(this).attr('id');
+
+        console.log(btnId);
+
+        if(btnId =='about-menu'){
+            scrollTo = aboutSection;
+        } else if(btnId =='services-menu'){
+            scrollTo = serviceSection;
+        }else if(btnId =='contact-menu'){
+            scrollTo = contactSection;
+        }else{
+            scrollTo = bannerSection;
+        }
+
+        $([document.documentElement,document.body]).animate({
+            scrollTop:$(scrollTo).offset().top - 70
+        },1000);
+    });
+   
+
+});
